031ac1c3fb34f4a6801771bcb619c5eb6e3e4774
g.bon <g.bon@student.isartdigital.com>
2023-03-27 19:45:44 +0200

    test Lfs

---
+++ Assets/Source/Script/TestLfs.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLfs : MonoBehaviour
+{
+
+    [SerializeField]
+    private float qsd;
+     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
