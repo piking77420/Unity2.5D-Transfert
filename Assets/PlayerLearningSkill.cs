bd6fc87fb7a248f6e5e7eadf1dab81ef807fea47
g.bon <g.bon@student.isartdigital.com>
2023-04-27 13:03:36 +0200

    wip sound

---
+++ Assets/PlayerLearningSkill.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLearningSkill : MonoBehaviour
+{
+    private enum PlayerSkill 
+    {
+        Translate , DragObject , ThroWEnemy,
+
+    }
+
+    private PlayerSkill skillToLearn;
+
+
+
+
+    private void FixedUpdate()
+    {
+        Collider
+    }
+}
