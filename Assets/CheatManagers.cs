2eceb59dda935c1f5edc5e36be75f7f631f858fb
g.bon <g.bon@student.isartdigital.com>
2023-05-01 08:58:29 +0200

    Cheat Engine Wip

--- Assets/CheatManagers.cs
+++ Assets/CheatManagers.cs
@@ -1,9 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class CheatManagers : MonoBehaviour
 {
+
+
+    [SerializeField]
+    private PlayerInput m_PlayerInput;
+
+    
+
+
+    private void Awake()
+    {
+        m_PlayerInput = GetComponent<PlayerInput>();
+    }
+
+
+
+    //Athenais
+    private void ChangeToVillage(InputAction.CallbackContext _callbackContext) 
+    {
+        if (_callbackContext.performed) 
+        {
+
+        }
+    }
+
+    // ETHAN
+    private void ChangeToVillage2(InputAction.CallbackContext _callbackContext)
+    {
+        if (_callbackContext.performed)
+        {
+
+        }
+    }
+
+    // Leonard
+    private void ChangeToCountry(InputAction.CallbackContext _callbackContext)
+    {
+        if (_callbackContext.performed)
+        {
+
+        }
+    }
+
+    //Leo
+    private void ChangeToForest(InputAction.CallbackContext _callbackContext)
+    {
+        if (_callbackContext.performed)
+        {
+
+        }
+    }
+
 
 
 
